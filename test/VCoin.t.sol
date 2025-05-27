a2e9d35780e529129a8f45c63af88f3f8a6e2384
Vignesh D <vignesh.d9123@gmail.com>
2025-05-27 12:26:57 +0530

    Add more tests for allowance

--- test/VCoin.t.sol
+++ test/VCoin.t.sol
@@ -27,7 +27,33 @@ contract TestVCoin is Test {
         c.transfer(msg.sender, 50);
         assertEq(c.balanceOf(msg.sender), 50);
         assertEq(c.balanceOf(address(this)), 50);
-
     }
+
+    // function testRevertMint() public {
+    //     c.mint(address(this), 100);
+    //     // vm.expectRevert();
+    //     assert(c.balanceOf(address(this)) ==  100);
+    // }
+
+    
+
+    function testAllowance() public{
+        c.mint(address(this), 100);
+        assertEq(c.balanceOf(address(this)), 100);
+
+        c.approve( 0x4C590bCBeB15093f2C5bdB4F460e9F9f90a4046C, 100);
+        vm.prank(0x4C590bCBeB15093f2C5bdB4F460e9F9f90a4046C);
+        c.transferFrom(address(this),0x2966473D85A76A190697B5b9b66b769436EFE8e5, 10 );
+        assertEq(c.balanceOf(0x2966473D85A76A190697B5b9b66b769436EFE8e5), 10);
+    }
+
+    function testRevertAllowance() public{
+        c.mint(address(this), 1000);
+        c.approve(0x4C590bCBeB15093f2C5bdB4F460e9F9f90a4046C, 100);
+        vm.expectRevert();
+        c.transferFrom(address(this), 0x2966473D85A76A190697B5b9b66b769436EFE8e5, 100); // This is called by address(this) itself not by 0x4C590bCBeB15093f2C5bdB4F460e9F9f90a4046C, hence fails and reverts
+    }
+
+
 
 }
